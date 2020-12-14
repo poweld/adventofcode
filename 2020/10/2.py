8acdd0870d1b22f74e16bbb63a5504c5e9af7e07
Dave Powell <poweld@gmail.com>
2020-12-14 10:32:33 -0500

    day 10.2 done, i hate it and don't have a better solution that completes

--- 2020/10/2.py
+++ 2020/10/2.py
@@ -16,31 +16,6 @@ def is_valid_joltage_input(joltage, adapter_rating):
   delta = adapter_rating - joltage
   return delta >= 1 and delta <= 3
 
-#def device_max_joltage_rating(adapter_ratings):
-#  return max(ratings) + 3
-#
-#def variation_to_str(variation):
-#  return ','.join([str(joltage) for joltage in variation])
-#
-#def get_valid_variations(adapter_ratings, start_at_index=1, seen_variation_strs=set()):
-#  variation_str = variation_to_str(adapter_ratings)
-#  print("len: {}".format(len(adapter_ratings)))
-#  print("starting at {}".format(start_at_index))
-#  if variation_str in seen_variation_strs:
-#    print("skip")
-#    return []
-#  seen_variation_strs.add(variation_str)
-#  valid_variations = [adapter_ratings]
-#  for i in range(start_at_index, len(adapter_ratings) - 3):
-#    if (is_valid_joltage_input(adapter_ratings[i], adapter_ratings[i + 2])):
-#      # can skip over the (i + 1) index
-#      new_adapters = adapter_ratings.copy()
-#      del new_adapters[i + 1]
-#      print("{}: found jump".format(adapter_ratings))
-#      valid_variations.extend(get_valid_variations(new_adapters, i, seen_variation_strs))
-#  print("done")
-#  return valid_variations
-
 def to_deltas(adapter_ratings):
   deltas = []
   for i in range(len(adapter_ratings) - 1):
@@ -54,47 +29,31 @@ def main():
   adapter_ratings.append(max(adapter_ratings) + 3) # device joltage
   adapter_ratings.sort()
   print(adapter_ratings)
-  skippable = set()
-  num_skips = 0
-  for i in range(len(adapter_ratings) - 3):
-    if is_valid_joltage_input(adapter_ratings[i], adapter_ratings[i + 2]):
-      skippable.add(adapter_ratings[i + 1])
-      num_skips = num_skips + 1
-    else:
-      continue
-    if is_valid_joltage_input(adapter_ratings[i], adapter_ratings[i + 3]):
-      skippable.add(adapter_ratings[i + 2])
-      num_skips = num_skips + 1
-  print("jumps found: {}, 2 ** jumps_found: {}".format(len(skippable), 2 ** len(skippable)))
-  print("num skips found: {}".format(num_skips))
   deltas = to_deltas(adapter_ratings)
   print("deltas: {}".format(deltas))
-  deltas_till_3_stack = []
-  deltas_till_3 = 0
+  packs_of_ones = []
+  ones_counter = 0
   for delta in deltas:
-    if deltas_till_3 + delta <= 3:
-      deltas_till_3 = deltas_till_3 + 1
+    if delta == 1:
+      ones_counter = ones_counter + 1
     else:
-      if deltas_till_3 > 1:
-        deltas_till_3_stack.append(deltas_till_3)
-      deltas_till_3 = 0
-  print(deltas_till_3_stack)
-  variations_mul = 1
-  variations_add = 0
+      if ones_counter > 0:
+        packs_of_ones.append(ones_counter)
+      ones_counter = 0
+  print("packs of ones: {}".format(packs_of_ones))
   variations = 1
-  for _ in range(len(deltas_till_3_stack)):
-    delta = deltas_till_3_stack.pop()
-    print("delta: {}".format(delta))
-    variations = variations + 1
-    variations_mul = variations_mul * delta
-    variations_add = variations_add + 1
-  print(variations_mul + variations_add)
-  #valid_variations = get_valid_variations(adapter_ratings)
-  #valid_variation_strings = [str.join(',', [str(vx) for vx in v]) for v in valid_variations]
-  #unique_variations = set(valid_variation_strings)
-  #for v in unique_variations:
-  #  print(v)
-  #print("valid variation count: {}".format(len(unique_variations)))
+  for pack_of_ones in packs_of_ones:
+    if pack_of_ones == 1:
+      # unnecessary but i hate this answer anyway so may as well include unnecessary ops
+      variations = variations * 1
+    elif pack_of_ones == 2:
+      variations = variations * 2
+    elif pack_of_ones == 3:
+      variations = variations * 4
+    elif pack_of_ones == 4:
+      variations = variations * 7
+  print(variations)
+
 
 if __name__ == '__main__':
   main()
