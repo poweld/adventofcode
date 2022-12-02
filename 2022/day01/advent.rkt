043fa52eee9ae49012aaf8017a0aec9fadffdf5d
Dave Powell <poweld@gmail.com>
2022-12-02 07:21:17 -0500

    2022 day01

--- 2022/day01/advent.rkt
+++ 2022/day01/advent.rkt
@@ -5,11 +5,13 @@
 (define testInputFilename "test_input.txt")
 
 (define input (open-input-file inputFilename))
-;(define testInput (open-input-file testInputFilename))
+(define testInput (open-input-file testInputFilename))
 
 ;(define words (string-split (read-line input)))
 ;(define nums (map (lambda (s) (string->number s)) words))
-(define nums (read-words-and-numbers/line inputFilename))
 (define words (read-words-and-numbers/line inputFilename))
+(define nums (read-words-and-numbers/line inputFilename))
+(define testWords (read-words-and-numbers/line testInputFilename))
+(define testNums (read-words-and-numbers/line testInputFilename))
 
-(provide words nums)
+(provide words nums testWords testNums)
--- 2022/day01/solution.rkt
+++ 2022/day01/solution.rkt
@@ -1,10 +1,43 @@
 #lang racket
-; (require racket/format)
-(require "../lib/advent.rkt")
+(require "advent.rkt")
 
 ; (+ 1 1)
 ; (define input (open-input-file "input.txt"))
 ; (define words (string-split (read-line input)))
 ; (define nums (map (lambda (s) (string->number s)) words))
 ; nums
-nums
+;nums
+;
+;(define ht (make-hash))
+;(hash-set! ht "apple" '("red" "round"))
+;(hash-set! ht "banana" '(yellow long))
+;(hash-ref ht "apple")
+;ht
+;(define ht2 (make-hash (list '("apple" red round))))
+;ht2
+;(map string-upcase (hash-ref ht "apple"))
+
+(define (group nums)
+  (define (groupRec nums groups currentGroup)
+    (if (empty? nums)
+      groups
+      (let ([head (first nums)]
+            [tail (rest nums)])
+        (if (empty? head)
+          (groupRec tail (append (list (reverse currentGroup)) groups) '())
+          (groupRec tail groups (append head currentGroup))))))
+  (reverse (groupRec nums '() '())))
+
+(define (sum nums)
+  (foldr + 0 nums))
+
+(define sums (map sum (group nums)))
+
+(define (max nums)
+  (last (sort nums <)))
+
+(define (max3 nums)
+  (take-right (sort nums <) 3))
+
+(max sums)
+(sum (max3 sums))
