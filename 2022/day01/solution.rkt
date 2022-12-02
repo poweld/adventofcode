a2df36e7a7322ec2f72557d0f58bdb8c12453f07
Dave Powell <poweld@gmail.com>
2022-12-02 11:59:04 -0500

    2022 day01 cleanup

--- 2022/day01/solution.rkt
+++ 2022/day01/solution.rkt
@@ -1,43 +1,22 @@
 #lang racket
-(require "advent.rkt")
-
-; (+ 1 1)
-; (define input (open-input-file "input.txt"))
-; (define words (string-split (read-line input)))
-; (define nums (map (lambda (s) (string->number s)) words))
-; nums
-;nums
-;
-;(define ht (make-hash))
-;(hash-set! ht "apple" '("red" "round"))
-;(hash-set! ht "banana" '(yellow long))
-;(hash-ref ht "apple")
-;ht
-;(define ht2 (make-hash (list '("apple" red round))))
-;ht2
-;(map string-upcase (hash-ref ht "apple"))
-
-(define (group nums)
-  (define (groupRec nums groups currentGroup)
-    (if (empty? nums)
-      groups
-      (let ([head (first nums)]
-            [tail (rest nums)])
-        (if (empty? head)
-          (groupRec tail (append (list (reverse currentGroup)) groups) '())
-          (groupRec tail groups (append head currentGroup))))))
-  (reverse (groupRec nums '() '())))
 
 (define (sum nums)
   (foldr + 0 nums))
 
-(define sums (map sum (group nums)))
+(define (load-data path)
+  (let ([lines (file->lines path)])
+    (for/fold ([acc '()]
+               [current-group '()]
+               #:result (reverse acc))
+              ([line (in-list lines)])
+      (if (non-empty-string? line)
+        (values acc (cons (string->number line) current-group))
+        (values (cons (reverse current-group) acc) '())))))
 
-(define (max nums)
-  (last (sort nums <)))
+(define input-file "input.txt")
 
-(define (max3 nums)
-  (take-right (sort nums <) 3))
+(define sorted-sums
+  (sort (map sum (load-data input-file)) <))
 
-(max sums)
-(sum (max3 sums))
+(last sorted-sums)
+(sum (take-right sorted-sums 3))
