9788e7763c24eb33eff651e440d487f6cae894d7
Dave Powell <poweld@gmail.com>
2022-12-03 02:06:15 -0500

    2022 day03 p1 raw

--- 2022/day03/solution.rkt
+++ 2022/day03/solution.rkt
@@ -16,10 +16,36 @@
       (+ (- ord ord-A) 27))))
 
 (define (split-rucksack rucksack)
+  ; Split the rucksack into two evenly sized compartments
   (let ([split-len (/ (length rucksack) 2)])
     (split-at rucksack split-len)))
 
-(split-rucksack '(1 2 3 4 5 6))
+(define (load-data path)
+  (let ([lines (file->lines path)])
+    (map string->list lines)))
+
+(define input-path "input.txt")
+
+(define rucksacks (load-data input-path))
+
+;(load-data input-path)
+(for/fold ([acc 0])
+          ([rucksack (in-list rucksacks)])
+  (let-values ([(comp1 comp2) (split-rucksack rucksack)])
+    (let* ([comp1-set (list->set comp1)]
+           [comp2-set (list->set comp2)]
+           [comp-intersect (first (set->list (set-intersect comp1-set comp2-set)))])
+      (begin
+        (printf "Rucksack: ~s\n" rucksacks)
+        (printf "Compartment 1: ~a\n" comp1)
+        (printf "Compartment 1 Set: ~a\n" comp1-set)
+        (printf "Compartment 2: ~a\n" comp2)
+        (printf "Compartment 2 Set: ~a\n" comp2-set)
+        (printf "Misplaced item type: ~a, priority: ~a\n" comp-intersect (priority comp-intersect))
+        (+ acc (priority comp-intersect))))))
+
+
+;(split-rucksack '(1 2 3 4 5 6))
 ;(split-at '(1 2 3 4 5 6) (/ (length )
 
 ;(priority #\a)
