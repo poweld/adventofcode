c121619a330179c8c1ffa515257552a23fa396cb
Dave Powell <poweld@gmail.com>
2023-12-06 13:13:38 -0500

    2023 day7 rust

--- 2023/rust/day05/part2/src/aoc.rs
+++ 2023/rust/day05/part2/src/aoc.rs
@@ -1,7 +1,80 @@
-use std::collections::HashMap;
 use std::ops::Range;
 
-#[derive(Debug)]
+enum OverlapResult {
+    OverlapLeft,
+    OverlapFull,
+    OverlapRight,
+    NoOverlap,
+}
+
+fn overlap(range1: &Range<u64>, range2: &Range<u64>) -> OverlapResult {
+        /*
+        [ and ) may be in the same location!
+        OverlapFull: [-----)
+                       [-)
+
+        OverlapLeft:      [-----)
+                     [-----)
+
+        OverlapRight: [-----)
+                           [-----)
+        NoOverlap: [-----)
+                         [-----)
+        */
+        //dbg!("overlap(", &range1, &range2, ")");
+        if range2.start == range2.end {
+            if range1.contains(&range2.start) {
+               OverlapResult::OverlapFull
+            } else {
+               OverlapResult::NoOverlap
+            }
+        } else {
+            if range1.contains(&range2.start) &&
+               range1.contains(&(range2.end - 1)) {
+               OverlapResult::OverlapFull
+            } else if range1.contains(&(range2.end - 1)) {
+               OverlapResult::OverlapLeft
+            } else if range1.contains(&range2.start) {
+               OverlapResult::OverlapRight
+            } else {
+               OverlapResult::NoOverlap
+            }
+        }
+}
+
+fn overlay(seed_range: &Range<u64>, source_range: &Range<u64>, dest_range: &Range<u64>) -> Vec<Range<u64>> {
+    match overlap(&seed_range, &source_range) {
+        OverlapResult::OverlapFull => {
+           vec![
+               Range { start: seed_range.start, end: source_range.start },
+               dest_range.clone(),
+               Range { start: source_range.end, end: seed_range.end },
+           ]
+        },
+        OverlapResult::OverlapLeft => {
+            // TODO bleh getting tired of thinking about this, but I think we're on the right track
+            let offset = seed_range.start - source_range.start;
+            let mapped_len = source_range.end - seed_range.start;
+            let dest_range_start = dest_range.start + offset;
+            vec![
+                Range { start: dest_range_start, end: dest_range_start + mapped_len },
+                Range { start: seed_range.start + mapped_len, end: seed_range.end },
+            ]
+        },
+        OverlapResult::OverlapRight => {
+            let offset = source_range.start - seed_range.start;
+            let mapped_len = seed_range.end - source_range.start;
+            let dest_range_start = dest_range.start + offset;
+            vec![
+            ]
+        },
+        OverlapResult::NoOverlap => {
+            todo!()
+        },
+    }
+}
+
+#[derive(Debug,Clone)]
 #[allow(dead_code)]
 struct Map {
     source_range: Range<u64>,
@@ -18,6 +91,44 @@ impl Map {
         let dest_range = dest_range_start..(dest_range_start + range_len);
         Self { source_range, dest_range }
     }
+    fn overlay(&self, other: &Self) -> Vec<Self> {
+        match overlap(&self.source_range, &other.source_range) {
+            OverlapResult::OverlapFull => {
+               vec![
+                   Map {
+                       source_range: Range { start: self.source_range.start, end: other.source_range.start },
+                       dest_range: self.dest_range.clone(),
+                   },
+                   other.clone(),
+                   Map {
+                       source_range: Range { start: other.source_range.end, end: self.source_range.end },
+                       dest_range: self.dest_range.clone(),
+                   },
+               ]
+            },
+            OverlapResult::OverlapLeft => {
+                vec![
+                    other.clone(),
+                    Map {
+                        source_range: Range { start: other.source_range.end, end: self.source_range.end },
+                        dest_range: self.dest_range.clone(),
+                    },
+                ]
+            },
+            OverlapResult::OverlapRight => {
+                vec![
+                    Map {
+                        source_range: Range { start: self.source_range.start, end: other.source_range.start },
+                        dest_range: self.dest_range.clone(),
+                    },
+                    other.clone(),
+                ]
+            },
+            OverlapResult::NoOverlap => {
+                vec![self.clone(), other.clone()]
+            },
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -33,8 +144,23 @@ impl Mapper {
         let maps = vec![];
         Self { maps }
     }
-    // fn map_to_category(&self, source_category: &str, source_id: u64, dest_category: &str, cache: &mut HashMap<CacheKey, u64>) -> u64 {
-    // }
+    fn lowest_mapped(&self, seed_ranges: &Vec<Range<u64>>) -> u64 {
+        seed_ranges.iter()
+            .map(|seed_range| {
+                self.maps.iter().map(|map| {
+                    dbg!(&seed_range, &map);
+                    dbg!(match overlap(&seed_range, &map.source_range) {
+                        OverlapResult::OverlapFull => map.dest_range.start,
+                        OverlapResult::OverlapLeft => map.dest_range.start + (seed_range.start - map.source_range.start),
+                        OverlapResult::OverlapRight => map.dest_range.start,
+        // TODO: almost there, but this isn't correct.
+                        OverlapResult::NoOverlap => seed_range.start,
+                    })
+                })
+                .min().unwrap()
+            })
+            .min().unwrap()
+    }
 }
 
 fn parse_seeds(seeds_line: &str) -> Vec<Range<u64>> {
@@ -45,7 +171,6 @@ fn parse_seeds(seeds_line: &str) -> Vec<Range<u64>> {
     let seeds_num_split: Vec<&str> = seed_num_strs.split_whitespace().collect();
     seeds_num_split[..].chunks(2)
         .map(|chunk| {
-            dbg!(&chunk);
             let seed_range_start = chunk[0].parse::<u64>().expect("could not parse seed range start");
             let seed_range_len = chunk[1].parse::<u64>().expect("could not parse seed range len");
             seed_range_start..(seed_range_start + seed_range_len)
@@ -54,53 +179,37 @@ fn parse_seeds(seeds_line: &str) -> Vec<Range<u64>> {
 }
 
 fn parse(input: &str) -> (Vec<Range<u64>>, Mapper) {
-    // TODO there's a LOT of seeds, may need to approach this differently
-    // Get all the ranges that can _lower_ the resulting value, along with _how much_ they lower the resulting value
-    // TODO or... take the initial set of map ranges and modify/add to them at each stage, so you end up with a single mapping
-    // ^^^^^^^^^^^^^^^^^^  though still not sure how to handle the fact that there's a ton of seeds to check...
     let mut lines = input.lines();
-    let seeds_line = lines.next();
     let seed_ranges = parse_seeds(lines.next().unwrap());
+    dbg!(&seed_ranges);
 
-    let mut mapper = Mapper::new();
     let mut maps: Vec<Map> = vec![];
     let is_map_line = |line: &&str| !(line.is_empty() || line.ends_with(&":"));
     for line in lines.filter(is_map_line) {
-        let map = Map::from_str(line);
-        let source_range = map.source_range;
-        for maps_index in 0..maps.len() {
-            let existing_source_range = maps[maps_index].source_range;
-            if (
-                existing_source_range.start == source_range.start &&
-                existing_source_range.end == source_range.end
-            ) {
-                maps[maps_index] = map;
-            } else if (
-                existing_source_range.contains(source_range.end - 1)
-                // TODO charlie is staring at me. need to update the existing range and possibly add new ones.
-            )
+        let new_map = Map::from_str(line);
+        // dbg!(&new_map);
+        if maps.is_empty() {
+            maps.push(new_map);
+        } else {
+            maps = maps.iter()
+                .flat_map(|map| map.overlay(&new_map))
+                //.flat_map(|map| dbg!(map.overlay(&new_map)))
+                .collect();
         }
+        // dbg!(&maps);
     }
     let mapper = Mapper { maps };
     (seed_ranges, mapper)
 }
 
-// TODO is this needed?
-fn range_overlap(range1: &Range<u64>, range2: &Range<u64>) -> Option<Range<u64>> {
-    if range2.start >= range1.start {
-        Some(Range { start: range2.start, end: range1.end })
-    } else if range1.start > range2.start {
-        Some(Range { start: range1.start, end: range2.end })
-    } else {
-        None
-    }
-}
-
 pub fn solve(input_path: &str) -> String {
     let input = std::fs::read_to_string(input_path).unwrap();
     let (seed_ranges, mapper) = parse(&input);
-    dbg!(&seed_ranges, &mapper);
-    todo!()
+    dbg!("here!");
+    // dbg!(&seed_ranges, &mapper);
+    // dbg!(&seed_ranges);
+    mapper.lowest_mapped(&seed_ranges)
+        .to_string()
 }
 
 #[cfg(test)]
---
+++ 2023/rust/day07/part1/src/aoc.rs
@@ -0,0 +1,191 @@
+use std::cmp::Ordering;
+use Ordering::{Less, Equal, Greater};
+
+static CARD_CHARS: &str = "0123456789TJQKA";
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct Card(u8);
+impl Card {
+    fn from(c: char) -> Self {
+        let card_value = CARD_CHARS.chars()
+            .position(|card_char| card_char == c)
+            .expect(&format!("failed to find card value for char '{c}'"));
+        Card(card_value as u8)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum HandType {
+    HighCard(Card),
+    OnePair(Card),
+    TwoPair(Card, Card),
+    ThreeOfAKind(Card),
+    FullHouse(Card, Card),  // It sure would be neat if this could instead contain a ThreeOfAKind and OnePair!
+    FourOfAKind(Card),
+    FiveOfAKind(Card),
+}
+use HandType::*;
+impl HandType {
+    fn from(cards: &[Card; 5]) -> Self {
+        let mut cards = cards.clone();
+        cards.sort();
+        let mut previous_card = None;
+        let mut matching_cards = vec![];
+        let mut current_matching_cards = vec![];
+        for card in cards.iter().rev() {
+            if let Some(previous_card) = previous_card {
+                if previous_card == card {
+                    current_matching_cards.push(card);
+                } else {
+                    matching_cards.push(current_matching_cards);
+                    current_matching_cards = vec![card];
+                }
+            } else {
+                current_matching_cards.push(card);
+            }
+            previous_card = Some(card);
+        }
+        matching_cards.push(current_matching_cards);
+        matching_cards.sort_by(|a, b| {
+            match a.len().cmp(&b.len()) {
+                Equal => a[0].cmp(&b[0]),
+                x => x,
+            }
+        });
+        let mut longest_two = matching_cards.iter().rev().take(2);
+        match [longest_two.next(), longest_two.next()] {
+            [Some(longest), Some(second_longest)] if longest.len() == 3 && second_longest.len() == 2 => {
+                FullHouse(longest[0].clone(), second_longest[0].clone())
+            },
+            [Some(longest), Some(second_longest)] if longest.len() == 2 && second_longest.len() == 2 => {
+                TwoPair(longest[0].clone(), second_longest[0].clone())
+            },
+            [Some(longest), None] => FiveOfAKind(longest[0].clone()),
+            [Some(longest), _] => match longest.len() {
+                4 => FourOfAKind(longest[0].clone()),
+                3 => ThreeOfAKind(longest[0].clone()),
+                2 => OnePair(longest[0].clone()),
+                1 => HighCard(longest[0].clone()),
+                _ => panic!("encountered unexpected length for longest matching card: {longest:?}"),
+            },
+            _ => panic!("could not find longest"),
+        }
+    }
+}
+impl PartialOrd for HandType {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for HandType {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self {
+            HighCard(_) => match other {
+                HighCard(_) => Equal,
+                _ => Less,
+            },
+            OnePair(_) => match other {
+                HighCard(_) => Greater,
+                OnePair(_) => Equal,
+                _ => Less,
+            },
+            TwoPair(..) => match other {
+                OnePair(_) | HighCard(_) => Greater,
+                TwoPair(..) => Equal,
+                _ => Less,
+            },
+            ThreeOfAKind(_) => match other {
+                OnePair(_) | HighCard(_) | TwoPair(..) => Greater,
+                ThreeOfAKind(_) => Equal,
+                _ => Less,
+            },
+            FullHouse(..) => match other {
+                OnePair(_) | HighCard(_) | TwoPair(..) | ThreeOfAKind(_) => Greater,
+                FullHouse(..) => Equal,
+                _ => Less,
+            },
+            FourOfAKind(_) => match other {
+                OnePair(_) | HighCard(_) | TwoPair(..) | ThreeOfAKind(_) | FullHouse(_, _) => Greater,
+                FourOfAKind(_) => Equal,
+                _ => Less,
+            },
+            FiveOfAKind(_) => match other {
+                FiveOfAKind(_) => Equal,
+                _ => Greater,
+            },
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Hand {
+    cards: [Card; 5],
+    hand_type: HandType,
+}
+impl Hand {
+    fn from(cards: &[Card; 5]) -> Self {
+        Self { cards: (*cards).clone(), hand_type: HandType::from(cards) }
+    }
+}
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+impl Ord for Hand {
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.hand_type.cmp(&other.hand_type) {
+            Equal => {
+                self.cards.iter()
+                    .zip(other.cards.iter())
+                    .find(|(a, b)| a.cmp(&b) != Equal)
+                    .map(|(a, b)| a.cmp(&b))
+                    .or(Some(Equal))
+                    .unwrap()
+            },
+            x => x,
+        }
+    }
+}
+
+fn parse(input: &str) -> Vec<(Hand, u64)> {
+    input.lines()
+        .map(|line| line.split_whitespace())
+        .map(|mut cards_and_bet_strs| {
+            let cards_str = cards_and_bet_strs.next().unwrap();
+            let cards: [Card; 5] = cards_str.chars()
+                .map(Card::from)
+                .collect::<Vec<_>>()
+                .try_into().expect(&format!("failed to convert into 5 cards: {cards_str}"));
+            let hand = Hand::from(&cards);
+            let bet_str = cards_and_bet_strs.last().unwrap();
+            let bet = bet_str.parse::<u64>().expect(&format!("failed to parse to u64: {bet_str}"));
+            (hand, bet)
+        })
+        .collect::<Vec<_>>()
+}
+
+pub fn solve(input_path: &str) -> String {
+    let input = std::fs::read_to_string(input_path).unwrap();
+    let mut hands_and_bets = parse(&input);
+    hands_and_bets.sort_by(|(hand1, _), (hand2, _)| hand1.cmp(&hand2));
+    hands_and_bets.iter()
+        .enumerate()
+        .map(|(index, (_, bet))| {
+            let rank = (index as u64) + 1;
+            bet * rank
+        })
+        .sum::<u64>()
+        .to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn solve_test() {
+        let result = solve("data/test_input.txt");
+        assert_eq!(result, 6440.to_string());
+    }
+}
