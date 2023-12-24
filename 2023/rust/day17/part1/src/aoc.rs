aafcee3e9c0f3994d32aa1e2bd0211f9779b211e
Dave Powell <poweld@gmail.com>
2023-12-24 00:00:43 -0500

    2023 day17 rust - wip

--- 2023/rust/day17/part1/src/aoc.rs
+++ 2023/rust/day17/part1/src/aoc.rs
@@ -1,6 +1,5 @@
 mod my {
-    // use std::collections::{HashSet, HashMap, VecDeque, BinaryHeap};
-    // use std::hash::Hash;
+    use std::collections::{BinaryHeap, VecDeque};
     use std::cmp::Ordering;
 
     #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -15,20 +14,21 @@ mod my {
     pub struct Edge {
         node: usize,
         cost: usize,
+        direction: Direction,
     }
 
     pub type Graph = Vec<Vec<Edge>>;
 
-    #[derive(Copy, Clone, PartialEq, Eq)]
+    #[derive(Clone, PartialEq, Eq)]
     struct State {
         cost: usize,
-        position: usize,
-        direction: Direction,
+        node: usize,
+        directions: VecDeque<Direction>,
     }
     impl Ord for State {
         fn cmp(&self, other: &Self) -> Ordering {
             other.cost.cmp(&self.cost)
-                .then_with(|| self.position.cmp(&other.position))
+                .then_with(|| self.node.cmp(&other.node))
         }
     }
     impl PartialOrd for State {
@@ -36,6 +36,50 @@ mod my {
             Some(self.cmp(other))
         }
     }
+
+    pub fn shortest_path(adj_list: &Vec<Vec<Edge>>, start: usize, goal: usize) -> Option<usize> {
+        let mut dist: Vec<_> = (0..adj_list.len()).map(|_| usize::MAX).collect();
+        let mut heap = BinaryHeap::new();
+
+        // We're at `start`, with a zero cost
+        dist[start] = 0;
+        heap.push(State { cost: 0, node: start, directions: VecDeque::new() });
+
+        // Examine the frontier with lower cost nodes first (min-heap)
+        while let Some(State { cost, node, directions }) = heap.pop() {
+            // Alternatively we could have continued to find all shortest paths
+            if node == goal { return Some(cost); }
+
+            if directions.len() == 3 && directions.iter().all(|d| d == &directions[0]) {
+                continue;
+            }
+
+            // Important as we may have already found a better way
+            if cost > dist[node] { continue; }
+
+            // For each node we can reach, see if we can find a way with
+            // a lower cost going through this node
+            for edge in &adj_list[node] {
+                let mut directions = directions.clone();
+                directions.push_back(edge.direction.clone());
+                if directions.len() > 3 {
+                    directions.pop_front();
+                }
+                let next = State { cost: cost + edge.cost, node: edge.node, directions };
+
+                // If so, add it to the frontier and continue
+                if next.cost < dist[next.node] {
+                    heap.push(next.clone());
+                    // Relaxation, we have now found a better way
+                    dist[next.node] = next.cost;
+                }
+            }
+        }
+
+        // Goal not reachable
+        None
+    }
+
 
     pub fn parse(input: &str) -> Graph {
         let lines = input.lines();
@@ -50,16 +94,16 @@ mod my {
         let graph: Graph = (0..costs.len()).map(|node| {
             let mut edges: Vec<Edge> = Vec::new();
             if node % col_count != 0 {
-                edges.push(Edge { node: node - 1, cost: costs[node - 1] });
+                edges.push(Edge { node: node - 1, cost: costs[node - 1], direction: Direction::West });
             }
             if node % col_count != col_count - 1 {
-                edges.push(Edge { node: node + 1, cost: costs[node + 1] });
+                edges.push(Edge { node: node + 1, cost: costs[node + 1], direction: Direction::East });
             }
             if node >= col_count {
-                edges.push(Edge { node: node - col_count, cost: costs[node - col_count] });
+                edges.push(Edge { node: node - col_count, cost: costs[node - col_count], direction: Direction::North });
             }
             if node < (row_count - 1) * col_count {
-                edges.push(Edge { node: node + col_count, cost: costs[node + col_count] });
+                edges.push(Edge { node: node + col_count, cost: costs[node + col_count], direction: Direction::South });
             }
             edges
         }).collect();
@@ -73,7 +117,9 @@ use my::*;
 
 pub fn solve(input_path: &str) -> String {
     let input = std::fs::read_to_string(input_path).unwrap();
-    dbg!(parse(&input));
+    let graph = parse(&input);
+    dbg!(shortest_path(&graph, 0, graph.len() - 1));
+    dbg!(shortest_path(&graph, 0, 5));
     todo!()
 }
 
