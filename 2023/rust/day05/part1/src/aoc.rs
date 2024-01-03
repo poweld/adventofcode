879e268d52c54dadcf7a373ace6f970d8bef3cd8
Dave Powell <poweld@gmail.com>
2024-01-02 19:00:56 -0500

    2023 day5 rust - cleanup

--- 2023/rust/day05/part1/src/aoc.rs
+++ 2023/rust/day05/part1/src/aoc.rs
@@ -1,51 +1,19 @@
-use std::collections::HashMap;
 use std::ops::Range;
 
 #[derive(Debug)]
-#[allow(dead_code)]
 struct Map {
-    source_category: String,
-    dest_category: String,
     source_range: Range<u64>,
     dest_range: Range<u64>,
 }
-
-#[derive(Debug)]
-struct Mapper {
-    category_maps: HashMap<String, Vec<Map>>,
-}
-impl Mapper {
-    fn new() -> Self {
-        let category_maps = HashMap::new();
-        Self { category_maps }
-    }
-    fn map_to_category(&self, source_category: &str, source_id: u64, dest_category: &str) -> u64 {
-        if source_category == dest_category {
-            source_id
-        } else {
-            let category_maps = self.category_maps.get(source_category)
-                .expect("failed to find source category");
-            let category_map = category_maps.iter()
-                .find(|m| m.source_range.contains(&source_id));
-            match category_map {
-                Some(category_map) => {
-                    let range_offset = source_id - category_map.source_range.start;
-                    let new_source_id = category_map.dest_range.start + range_offset;
-                    self.map_to_category(&category_map.dest_category, new_source_id, dest_category)
-                },
-                None => {
-                    // TODO uh oh, don't have a dest category to pull from when no mapping was added
-                    // does this need to be statically added?
-                    // HACK IT FOR NOW
-                    let category_map = category_maps.iter().next().expect("couldn't find a single category map");
-                    self.map_to_category(&category_map.dest_category, source_id, dest_category)
-                }
-            }
-        }
+impl Map {
+    fn process(&self, item: u64) -> Option<u64> {
+        if self.source_range.contains(&item) {
+            Some(self.dest_range.start + item - self.source_range.start)
+        } else { None }
     }
 }
 
-fn parse(input: &str) -> (Vec<u64>, Mapper) {
+fn parse(input: &str) -> (Vec<u64>, Vec<Vec<Map>>) {
     let mut lines = input.lines();
     let seeds_line = lines.next();
     let seed_num_strs = seeds_line
@@ -56,55 +24,46 @@ fn parse(input: &str) -> (Vec<u64>, Mapper) {
     let seeds: Vec<u64> = seed_num_strs.split_whitespace()
         .map(|num_str| num_str.parse::<u64>().expect("failed to parse num"))
         .collect();
-    lines.next();  // Consume empty line
+    lines.next(); lines.next();
 
-    let mut mapper = Mapper::new();
-    let mut source_category = String::new();
-    let mut dest_category = String::new();
-    let mut definition_line = true;
-    let mut maps: Vec<Map> = vec![];
-    for line in lines {
+    let mut map_stages: Vec<Vec<Map>> = Vec::new();
+    let mut current_map_stage: Vec<Map> = Vec::new();
+    while let Some(line) = lines.next() {
         if line.is_empty() {
-            mapper.category_maps.insert(source_category.clone(), maps);
-            maps = vec![];
-            definition_line = true;
-        } else if definition_line {
-            let mut definition_parts = line.split_whitespace()
-                .next().expect("failed to get first part of definition line")
-                .split("-");
-            source_category = definition_parts.next().expect("failed to get source category").to_string();
-            definition_parts.next();
-            dest_category = definition_parts.next().expect("failed to get dest category").to_string();
-            definition_line = false;
-        } else {
-            let mut nums = line.split_whitespace()
-                .map(|num_str| num_str.parse::<u64>().expect("failed to parse num"));
-            let dest_range_start = nums.next().expect("failed to get dest_range_start str");
-            let source_range_start = nums.next().expect("failed to get source_range_start str");
-            let range_len = nums.next().expect("failed to get range_len str");
-            let source_range = source_range_start..(source_range_start + range_len);
-            let dest_range = dest_range_start..(dest_range_start + range_len);
-            let map = Map {
-                source_category: source_category.clone(),
-                dest_category: dest_category.clone(),
-                source_range,
-                dest_range
-                };
-            maps.push(map);
+            map_stages.push(current_map_stage);
+            current_map_stage = Vec::new();
+            lines.next();
+            continue;
         }
-    }
-    mapper.category_maps.insert(source_category.clone(), maps);
+        let map_parts: Vec<u64> = line.split_whitespace()
+            .map(|num_str| num_str.parse::<u64>().expect("failed to parse num"))
+            .collect();
+        let dest_range_start = map_parts[0];
+        let source_range_start = map_parts[1];
+        let range_len = map_parts[2];
+        let map = Map {
+            source_range: source_range_start..(source_range_start + range_len),
+            dest_range: dest_range_start..(dest_range_start + range_len),
+        };
+        current_map_stage.push(map);
 
-    (seeds, mapper)
+    }
+    map_stages.push(current_map_stage);
+    (seeds, map_stages)
 }
 
 pub fn solve(input_path: &str) -> String {
     let input = std::fs::read_to_string(input_path).unwrap();
-    let (seeds, mapper) = parse(&input);
-    seeds.iter()
-        .map(|seed| mapper.map_to_category(&"seed", *seed, &"location"))
-        .min().expect("could not get min")
-        .to_string()
+    let (seeds, map_stages) = parse(&input);
+    let results = seeds.into_iter().map(|seed| {
+        map_stages.iter().fold(seed, |acc, map_stage| {
+            map_stage.iter().map(|map| map.process(acc))
+                .find(|result| result.is_some())
+                .flatten()
+                .unwrap_or(acc)
+        })
+    });
+    results.min().unwrap().to_string()
 }
 
 #[cfg(test)]
