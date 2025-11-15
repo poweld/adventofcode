defca8975e276728a90c6de884765956e77425a7
Dave Powell <poweld@gmail.com>
2025-11-15 14:16:48 -0500

    2025 - prep

--- 2025/python/day01/main.py
+++ 2025/python/day01/main.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 INPUT = "../../../input/2024/day01/input.txt"
 TEST_INPUT = "../../../input/2024/day01/test_input.txt"
 
@@ -13,7 +15,7 @@ def parse_input(input):
     return [(int(left_value), int(right_value)) for (left_value, right_value) in split]
 
 
-def main():
+def part_1():
     parsed_input = parse_input(read_input())
     acc = ([], [])
     [
@@ -27,5 +29,23 @@ def main():
     print(sum(distances))
 
 
+def part_2():
+    parsed_input = parse_input(read_input())
+    acc = ([], [])
+    [
+        acc := (acc[0] + [left_value], acc[1] + [right_value])
+        for (left_value, right_value) in parsed_input
+    ]
+    (left, right) = acc
+    right_count = Counter()
+    for right_value in right:
+        right_count[right_value] += 1
+    left.sort()
+    right.sort()
+    distances = [left_value * right_count[left_value] for left_value in left]
+    print(sum(distances))
+
+
 if __name__ == "__main__":
-    main()
+    part_1()
+    part_2()
